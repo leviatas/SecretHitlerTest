bcb96c0e1e30b2c1200bd21a2a88511e23875a21
leviatas <31702513+leviatas@users.noreply.github.com>
2017-09-20 10:15:05 -0300

    Update Commands.py

--- Commands.py
+++ Commands.py
@@ -212,15 +212,15 @@ def command_showhistory(bot, update):
       cid = update.message.chat_id
       bot.send_message(cid, "Looking for history...")
       #Check if there is a current game 
-      #if cid in GamesController.games.keys():
-      game = GamesController.games.get(cid, None)    
-      history.append(1)
-      history_text = "The following commands are available:\n"
-      for i in game.history:
-          history_text += i + "\n"
-      bot.send_message(cid, history_text)
-      #else:
-      #  bot.send_message(cid, "There is no game in this chat. Create a new game with /newgame")
+      if cid in GamesController.games.keys():
+        game = GamesController.games.get(cid, None)    
+        history.append(1)
+        history_text = "The following commands are available:\n"
+        for i in game.history:
+            history_text += i + "\n"
+        bot.send_message(cid, history_text)
+      else:
+        bot.send_message(cid, "There is no game in this chat. Create a new game with /newgame")
     except Exception as e:
       bot.send_message(cid, str(e))
         #log.error("Unknown error: " + str(e))
