33d0747984f996b502db6a96e28d902c9e1c8461
leviatas <31702513+leviatas@users.noreply.github.com>
2017-09-21 11:20:27 +0200

    Update Commands.py

--- Commands.py
+++ Commands.py
@@ -245,6 +245,7 @@ def command_showhistory(bot, update):
     try:
       #Send message of executing command   
       cid = update.message.chat_id
+      game.pedrote = 3
       #bot.send_message(cid, "Looking for history...")
       bot.send_message(cid, "Debug info")
       bot.send_message(cid, "Current chat id: " + str(cid))
