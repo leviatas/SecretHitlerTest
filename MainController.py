74d6e68d9001049833218ecb37b9342ace5bb83c
leviatas <31702513+leviatas@users.noreply.github.com>
2017-09-27 12:46:37 -0300

    Update MainController.py

--- MainController.py
+++ MainController.py
@@ -759,7 +759,6 @@ def main():
         cur = conn.cursor()
         cur.execute(open("DBCreate.sql", "r").read())
         
-        #query = "INSERT INTO users (facebook_id, name , access_token , created) VALUES (\"1\", \"2\", \"3\", 1);"
         query = """INSERT INTO users(facebook_id, name , access_token , created)
              VALUES(%s) RETURNING id;"""
         cur.execute(sql, ("1","2","3",1))
