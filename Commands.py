c71e120efb3aaf724d7574ec91cf3c15d08bdb68
leviatas <31702513+leviatas@users.noreply.github.com>
2017-09-27 16:11:05 -0300

    Update Commands.py

--- Commands.py
+++ Commands.py
@@ -204,7 +204,7 @@ def encode_all(obj):
             return obj.__dict__
         if isinstance(obj, Board):
             return obj.__dict__            
-        return obj
+        return json.JSONEncoder.default(self,obj)
 			
 def command_startgame(bot, update):
 	log.info('command_startgame called')
