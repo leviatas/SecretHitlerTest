f12064f002f0c5bd18034c75b3525ef22c5b2db9
leviatas <31702513+leviatas@users.noreply.github.com>
2017-09-28 16:12:21 -0300

    Update Player.py

--- Boardgamebox/Player.py
+++ Boardgamebox/Player.py
@@ -6,3 +6,12 @@ class Player(object):
         self.party = None
         self.is_dead = False
         self.inspected_players = {}
+    def __init__(self, data):
+        for name, value in data.items():
+            setattr(self, name, self._wrap(value))
+
+    def _wrap(self, value):
+        if isinstance(value, (tuple, list, set, frozenset)): 
+            return type(value)([self._wrap(v) for v in value])
+        else:        
+            return Player(value) if isinstance(value, dict) else value
