166ab13cc38a91145158b6250107c475164cd2b9
leviatas <31702513+leviatas@users.noreply.github.com>
2017-09-28 14:12:01 -0300

    Update Game.py

--- Boardgamebox/Game.py
+++ Boardgamebox/Game.py
@@ -10,7 +10,17 @@ class Game(object):
         self.board = None
         self.initiator = initiator
         self.dateinitvote = None
+    
+    def __init__(self, data):
+        for name, value in data.iteritems():
+            setattr(self, name, self._wrap(value))
 
+    def _wrap(self, value):
+        if isinstance(value, (tuple, list, set, frozenset)): 
+            return type(value)([self._wrap(v) for v in value])
+        else:
+            return Struct(value) if isinstance(value, dict) else value
+    
     def add_player(self, uid, player):
         self.playerlist[uid] = player
 
