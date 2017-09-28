6a6950976eb1aff11e49621936afd8a23bbb2b32
leviatas <31702513+leviatas@users.noreply.github.com>
2017-09-28 16:12:26 -0300

    Update Board.py

--- Boardgamebox/Board.py
+++ Boardgamebox/Board.py
@@ -11,6 +11,16 @@ class Board(object):
         self.policies = random.sample(policies, len(policies))
         self.discards = []
         self.previous = []
+        
+    def __init__(self, data):
+        for name, value in data.items():
+            setattr(self, name, self._wrap(value))
+
+    def _wrap(self, value):
+        if isinstance(value, (tuple, list, set, frozenset)): 
+            return type(value)([self._wrap(v) for v in value])
+        else:                  
+            return Board(value) if isinstance(value, dict) else value
 
     def print_board(self, player_sequence):
         board = "--- Liberal acts ---\n"
