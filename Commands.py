cb72fc9d78fa0a02c002967a8a465083244e3f85
leviatas <31702513+leviatas@users.noreply.github.com>
2017-09-28 13:13:11 -0300

    Update Commands.py

--- Commands.py
+++ Commands.py
@@ -175,7 +175,9 @@ def command_newgame(bot, update):
 			log.info(dbdata)
 			
 			if cur.rowcount > 0:
-				x = json.loads(str(dbdata[2]), object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
+				s1 = json.dumps(dbdata[2])
+				d2 = json.loads(s1)
+				x = json.loads(d2, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
 			else:
 				GamesController.games[cid] = Game(cid, update.message.from_user.id)
 			
