ae6893ab5b865b0322d91313da6c396c0d02d561
leviatas <31702513+leviatas@users.noreply.github.com>
2017-09-28 16:23:09 -0300

    Update State.py

--- Boardgamebox/State.py
+++ Boardgamebox/State.py
@@ -16,13 +16,4 @@ class State(object):
         self.player_counter = 0
         self.veto_refused = False
         self.not_hitlers = []
-        self.currentround = -1
-    def __init__(self, data):
-        for name, value in data.items():
-            setattr(self, name, self._wrap(value))
-
-    def _wrap(self, value):
-        if isinstance(value, (tuple, list, set, frozenset)): 
-            return type(value)([self._wrap(v) for v in value])
-        else:                  
-            return State(value) if isinstance(value, dict) else value
+        self.currentround = -1   
