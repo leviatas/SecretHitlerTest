ada94a9e79918b7650e33ce570459be8e3949032
leviatas <31702513+leviatas@users.noreply.github.com>
2017-10-04 09:48:20 -0300

    Update Commands.py

--- Commands.py
+++ Commands.py
@@ -175,6 +175,8 @@ def command_join(bot, update, args):
 		fname = update.message.from_user.first_name
 		uid = update.message.from_user.id
 	else:
+		log.info(str(cid))
+		log.info(str(ADMIN))
 		if cid == ADMIN:
 			# If args, testing behaviour				
 			fname = args[0]
