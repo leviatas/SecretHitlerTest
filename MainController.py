7e2a9aea8699f67a7bf4e08a6962821e855ad6cd
leviatas <31702513+leviatas@users.noreply.github.com>
2017-10-20 10:00:39 -0300

    Update MainController.py

--- MainController.py
+++ MainController.py
@@ -838,7 +838,7 @@ def main():
         dp.add_error_handler(error)
 
         # Start the Bot
-        updater.start_polling()
+        #updater.start_polling()
 
         # Run the bot until the you presses Ctrl-C or the process receives SIGINT,
         # SIGTERM or SIGABRT. This should be used most of the time, since
