980c941f6dd6d98288191d7658e0629e766ba5b1
leviatas <31702513+leviatas@users.noreply.github.com>
2017-10-20 09:30:05 -0300

    Update MainController.py

--- MainController.py
+++ MainController.py
@@ -80,8 +80,8 @@ def start_round(bot, game):
                 game.board.state.nominated_president = game.board.state.chosen_president
                 game.board.state.chosen_president = None
         bot.send_message(game.cid,
-                "The next presidential canditate is [%s](tg://user?id=%d).\n%s, please nominate a Chancellor in our private chat!" % 
-                         (game.board.state.nominated_president.name, game.board.state.nominated_president.uid, game.board.state.nominated_president.name), ParseMode.MARKDOWN))
+                text="The next presidential canditate is [%s](tg://user?id=%d).\n%s, please nominate a Chancellor in our private chat!" % 
+                         (game.board.state.nominated_president.name, game.board.state.nominated_president.uid, game.board.state.nominated_president.name), parse_mode=ParseMode.MARKDOWN))
         choose_chancellor(bot, game)
         # --> nominate_chosen_chancellor --> vote --> handle_voting --> count_votes --> voting_aftermath --> draw_policies
         # --> choose_policy --> pass_two_policies --> choose_policy --> enact_policy --> start_round
