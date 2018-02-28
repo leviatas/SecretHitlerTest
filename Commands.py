867cd7263f297b68716c5dd75494865db4e5195b
leviatas <31702513+leviatas@users.noreply.github.com>
2018-02-28 09:32:46 -0300

    Update Commands.py

--- Commands.py
+++ Commands.py
@@ -346,10 +346,16 @@ def command_showhistory(bot, update):
 			#bot.send_message(cid, "Current round: " + str(game.board.state.currentround + 1))
 			uid = update.message.from_user.id
 			history_text = "Historial:\n\n" 
+			history_textContinue = "" 
 			for x in game.history:
-				history_text += x + "\n\n"
+				if(len(history_text) < 3500)
+					history_text += x + "\n\n"
+				else
+					history_textContinue = += x + "\n\n"
 
 			bot.send_message(uid, history_text, ParseMode.MARKDOWN)
+			if(len(history_textContinue) > 0)
+				bot.send_message(uid, history_textContinue, ParseMode.MARKDOWN)
 			#bot.send_message(cid, "I sent you the history to our private chat")			
 		else:
 			bot.send_message(cid, "No hay juego en este chat. Crea un nuevo juego con /newgame")
