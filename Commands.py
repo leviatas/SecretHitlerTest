9e84401bb21c345b3c7fa3e6b6d5d5f5e1703165
leviatas <31702513+leviatas@users.noreply.github.com>
2018-12-18 09:17:02 -0300

    Update Commands.py

--- Commands.py
+++ Commands.py
@@ -158,16 +158,8 @@ def command_stats(bot, update, args):
 			jugador = ' '.join(args)
 			#query = "select x.game_endcode, COUNT(case when REPLACE (x.playerlist, ' (dead)', '') like '%%" + jugador + " secret role was Fasc%%' then x.game_endcode end), COUNT(case when REPLACE (x.playerlist, ' (dead)', '') like '%%" + jugador + " secret role was Hitl%%' then x.game_endcode end), COUNT(case when REPLACE (x.playerlist, ' (dead)', '') like '%%" + jugador + " secret role was Libe%%' then x.game_endcode end)  FROM stats_detail x where REPLACE (x.playerlist, ' (dead)', '') like '%%" + jugador + " secret role was%%' GROUP BY game_endcode"
 			
-			#replace_dead = regexp_replace(playerlist, ' \(dead\)| \(muerto\)', '', 'g')
-			'''
-			query = "select x.game_endcode, COUNT(case when REPLACE (x.playerlist, ' (dead)', '') like " \
-				"'%%" + jugador + " secret role was Fasc%%' then x.game_endcode end)," \
-				"COUNT(case when REPLACE (x.playerlist, ' (dead)', '') like " \
-				"'%%" + jugador + " secret role was Hitl%%' then x.game_endcode end)," \
-				"COUNT(case when REPLACE (x.playerlist, ' (dead)', '') like " \
-				"'%%" + jugador + " secret role was Libe%%' then x.game_endcode end) " \
-				"FROM stats_detail x where REPLACE (x.playerlist, ' (dead)', '') like " \
-				"'%%" + jugador + " secret role was%%' GROUP BY game_endcode"
+			replace_dead = "regexp_replace(playerlist, ' \(dead\)| \(muerto\)', '', 'g')"
+			
 			'''
 			query = "select x.game_endcode, COUNT(case when REPLACE (x.playerlist, ' (dead)', '') like " \
 				"'%%{0} secret role was Fasc%%' then x.game_endcode end)," \
@@ -178,6 +170,16 @@ def command_stats(bot, update, args):
 				"FROM stats_detail x where REPLACE (x.playerlist, ' (dead)', '') like " \
 				"'%%{0} secret role was%%' GROUP BY game_endcode" \
 				.format(jugador)
+			'''
+			query = "select x.game_endcode, COUNT(case when {1} like " \
+				"'%%{0} secret role was Fasc%%' then x.game_endcode end)," \
+				"COUNT(case when {1} like " \
+				"'%%{0} secret role was Hitl%%' then x.game_endcode end)," \
+				"COUNT(case when {1} like " \
+				"'%%{0} secret role was Libe%%' then x.game_endcode end) " \
+				"FROM stats_detail x where {1} like " \
+				"'%%{0} secret role was%%' GROUP BY game_endcode" \
+				.format(jugador, replace_dead)
 			
 			#query = "INSERT INTO games(id , groupName  , data) VALUES (%s, %s, %s) RETURNING data;"
 			cursor.execute(query)
