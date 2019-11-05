55e1d80d04a564cdf355d81bade694ae472e0d35
Peluffo Eduardo <ePeluffo@cas.austral.edu.ar>
2019-11-05 09:52:11 -0300

    fixing

--- MainController.py
+++ MainController.py
@@ -1073,34 +1073,34 @@ def getGamesByTipo(opcion):
 
 def error_callback(update, context):
 	# add all the dev user_ids in this list. You can also add ids of channels or groups.
-    devs = [ADMIN]    
-    # This traceback is created with accessing the traceback object from the sys.exc_info, which is returned as the
-    # third value of the returned tuple. Then we use the traceback.format_tb to get the traceback as a string, which
-    # for a weird reason separates the line breaks in a list, but keeps the linebreaks itself. So just joining an
-    # empty string works fine.
-    trace = "".join(traceback.format_tb(sys.exc_info()[2]))
-    # lets try to get as much information from the telegram update as possible
-    payload = ""
-    # normally, we always have an user. If not, its either a channel or a poll update.
-    if update.effective_user:
-        payload += f' with the user {mention_html(update.effective_user.id, update.effective_user.first_name)}'
-    # there are more situations when you don't get a chat
-    if update.effective_chat:
-        payload += f' within the chat <i>{update.effective_chat.title}</i>'
-        if update.effective_chat.username:
-            payload += f' (@{update.effective_chat.username})'
-    # but only one where you have an empty payload by now: A poll (buuuh)
-    if update.poll:
-        payload += f' with the poll id {update.poll.id}.'
-    # lets put this in a "well" formatted text
-    text = f"Hey.\n The error <code>{context.error}</code> happened{payload}. The full traceback:\n\n<code>{trace}" \
-           f"</code>"
-    # and send it to the dev(s)
-    for dev_id in devs:
-        context.bot.send_message(dev_id, text, parse_mode=ParseMode.HTML)
-    # we raise the error again, so the logger module catches it. If you don't use the logger module, use it.
+	devs = [ADMIN]    
+	# This traceback is created with accessing the traceback object from the sys.exc_info, which is returned as the
+	# third value of the returned tuple. Then we use the traceback.format_tb to get the traceback as a string, which
+	# for a weird reason separates the line breaks in a list, but keeps the linebreaks itself. So just joining an
+	# empty string works fine.
+	trace = "".join(traceback.format_tb(sys.exc_info()[2]))
+	# lets try to get as much information from the telegram update as possible
+	payload = ""
+	# normally, we always have an user. If not, its either a channel or a poll update.
+	if update.effective_user:
+		payload += f' with the user {mention_html(update.effective_user.id, update.effective_user.first_name)}'
+	# there are more situations when you don't get a chat
+	if update.effective_chat:
+		payload += f' within the chat <i>{update.effective_chat.title}</i>'
+	if update.effective_chat.username:
+		payload += f' (@{update.effective_chat.username})'
+	# but only one where you have an empty payload by now: A poll (buuuh)
+	if update.poll:
+		payload += f' with the poll id {update.poll.id}.'
+	# lets put this in a "well" formatted text
+	text = f"Hey.\n The error <code>{context.error}</code> happened{payload}. The full traceback:\n\n<code>{trace}" \
+	f"</code>"
+	# and send it to the dev(s)
+	for dev_id in devs:
+		context.bot.send_message(dev_id, text, parse_mode=ParseMode.HTML)
+	# we raise the error again, so the logger module catches it. If you don't use the logger module, use it.
 	logger.warning("User: {}.\n\nError {}.\n\nTrace: {}".format(update.effective_user.first_name, context.error, trace))
-
+	
 def change_groupname(bot, update):
 	cid = update.message.chat.id
 	groupname = update.message.chat.title
